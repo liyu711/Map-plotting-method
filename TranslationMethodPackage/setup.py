3f7db11105a051afa34fdcfdf7c72da36817448c
Yudong Li <yudongli@YudongdeMacBook-Pro.local>
2018-05-16 11:18:35 -0400

    first commit

---
+++ TranslationMethodPackage/setup.py
@@ -0,0 +1,20 @@
+# This is the setup file for pip
+from setuptools import setup, find_packages
+
+setup(name='TranslationMethod',
+
+      version='0.1',
+
+      description='Translate Depth Map into 3D map',
+
+      url='placeholder',
+
+      author='Yudong Li',
+
+      author_email='a1923172548@gmail.com',
+
+      license='MIT',
+
+      packages=find_packages(),
+
+      zip_safe=False)
