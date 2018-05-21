405391f85ab5a7fa351d0795f7854bda05259c73
Yudong Li <yudongli@YudongdeMacBook-Pro.local>
2018-05-21 14:14:15 -0400

    move_obstacle method, debugging pending

--- TranslationMethodPackage/TranslationMethod/obstacle.py
+++ TranslationMethodPackage/TranslationMethod/obstacle.py
@@ -7,6 +7,7 @@ class Obstacle(object):
 		# center: 3D array representing a point 
 		self.side_length = side_length
 		self.center = center
+		self.current_point = center
 		self.boundary_points = [
 			numpy.array([center[0]-side_length/2, center[1]-side_length/2, center[2]-side_length/2]),
 			numpy.array([center[0]-side_length/2, center[1]-side_length/2, center[2]+side_length/2]),
@@ -19,10 +20,19 @@ class Obstacle(object):
 		]
 
 	def get_side(self):
-		return side_length
+		return self.side_length
 
 	def get_center(self):
-		return center
+		return self.center
+
+	def get_current_point(self):
+		return self.current_point
+
+	def set_current_point(self, point):
+		self.current_point = point
+
+	def reset_current_point(self):
+		self.current_point = self.center
 
 	def check_if_point_is_inside_the_obstacle(self, point_to_check):
 		status = False
@@ -33,3 +43,27 @@ class Obstacle(object):
 			status = True
 
 		return status
+
+	def move(self, fin_point):
+		def move(self):
+		current_x = self.current_point[0]
+		current_y = self.current_point[1]
+		min_magnitude = 10000000000000
+		point_to_go = self.current_point
+		where_should_i_go = [
+			numpy.array([current_x -1, current_y +1]),
+			numpy.array([current_x -1, current_y]),
+			numpy.array([current_x -1, current_y -1]),
+			numpy.array([current_x, current_y +1]),
+			numpy.array([current_x, current_y -1]),
+			numpy.array([current_x +1, current_y +1]),
+			numpy.array([current_x +1, current_y]),
+			numpy.array([current_x +1, current_y -1])
+		]
+		for point in where_should_i_go:
+			distance_magnitude_to_the_end = VectorMath.get_magnitude(fin_point, point)
+			if distance_magnitude_to_the_end < min_magnitude:
+				distance_magnitude_to_the_end = min_magnitude
+				point_to_go = point
+
+		self.set_current_point(point_to_go)
